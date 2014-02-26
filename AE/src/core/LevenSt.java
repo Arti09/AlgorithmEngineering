bcff3cdd1e19dc594f513678a0f153ea8ea796c0
Arti <artem.pokas@gmx.de>
2014-02-26 23:49:39 +0100

    Benchmark added

--- AE/src/core/LevenSt.java
+++ AE/src/core/LevenSt.java
@@ -1,9 +1,45 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class LevenSt {
-	public int getLevenSt(String s1, String s2) {
-		return (StringUtils.getLevenshteinDistance(s1, s2));
+    List<String> liste = new ArrayList<String>();
+
+    public int getLevenSt(String s1, String s2) {
+	return (StringUtils.getLevenshteinDistance(s1, s2));
+    }
+
+    public void addString(String toConvert) {
+	liste.add(toConvert);
+    }
+
+    public List<String> getList() {
+	return liste;
+    }
+
+    public int calculateDiffrence(List<String> listeOther) {
+	int difference = 0;
+	for (int i = 0; i < liste.size(); i++) {
+	    if (listeOther.size() > i) {
+		difference += StringUtils.getLevenshteinDistance(liste.get(i),
+			listeOther.get(i));
+	    } else {
+		difference += StringUtils.getLevenshteinDistance(liste.get(i),
+			"");
+	    }
 	}
+
+	if (listeOther.size() > liste.size()) {
+	    for (int i = liste.size(); i < listeOther.size(); i++) {
+		difference += StringUtils.getLevenshteinDistance("",
+			listeOther.get(i));
+	    }
+	}
+
+	return difference;
+    }
+
 }
--- AE/src/test/TestConversion.java
+++ AE/src/test/TestConversion.java
@@ -10,123 +10,169 @@ import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 
 import core.LevenSt;
 import core.MetaphoneAlg;
 import core.SoundexAlg;
 
+@BenchmarkOptions(callgc = false, benchmarkRounds = 200, warmupRounds = 3)
 public class TestConversion {
-	List<String> liste = new ArrayList<String>();
-	List<String> toCompare = new ArrayList<String>();
-	int i = 4;
-	int test = 0;
+    
+    @Rule
+    public TestRule benchmarkRun = new BenchmarkRule();
+     
+    List<String> liste = new ArrayList<String>();
+    List<String> toCompare = new ArrayList<String>();
+    int i = 4;
+    int test = 0;
 
-	@Before
-	public void onBefore() { 
-		System.out.println("----- Start Test -----");
-		readFile(true);
-		readFile(false);
+    @Before
+    public void onBefore() {
+//	System.out.println("----- Start Test -----");
+	readFile(true);
+	readFile(false);
+    }
+
+    @After
+    public void onAfter() {
+	liste.clear();
+	toCompare.clear();
+//	System.out.println("----- Finish Test -----\n");
+    }
+
+    public void test() {
+	SoundexAlg alg = new SoundexAlg();
+	System.out.println("Unterschied Soundex = "
+		+ alg.compareStrings(liste.get(i), toCompare.get(i)));
+
+	LevenSt alg2 = new LevenSt();
+	System.out.println("Unterschied Levenshtein = "
+		+ alg2.getLevenSt(liste.get(i), toCompare.get(i)));
+
+	MetaphoneAlg alg3 = new MetaphoneAlg();
+	System.out.println("Unterschied Metaphone = "
+		+ alg3.getDiffrence(liste.get(i), toCompare.get(i)));
+    }
+
+    @Test
+    public void Metaphone() {
+	int amount1 = 0;
+	MetaphoneAlg alg = new MetaphoneAlg();
+	for (String str : liste) {
+	    for (String str2 : str.split(" ")) {
+		alg.getStringRepresantation(str2);
+		amount1++;
+	    }
+	}
+	int amount2 = 0;
+	MetaphoneAlg alg2 = new MetaphoneAlg();
+	for (String str : toCompare) {
+	    for (String str2 : str.split(" ")) {
+		alg2.getStringRepresantation(str2);
+		amount2++;
+	    }
+	}
+//	System.out.println("Anzahl 1 = " + amount1 + " Anzahl 2 = " + amount2);
+//	System.out.println("Unterschied = "
+//		+ alg.calculateDiffrence(alg2.getMap()));
+    }
+
+    @Test
+    public void Soundex() {
+	int amount1 = 0;
+	SoundexAlg alg = new SoundexAlg();
+	for (String str : liste) {
+	    for (String str2 : str.split(" ")) {
+		alg.getStringRepresantation(str2);
+		amount1++;
+	    }
+	}
+	int amount2 = 0;
+	SoundexAlg alg2 = new SoundexAlg();
+	for (String str : toCompare) {
+	    for (String str2 : str.split(" ")) {
+		alg2.getStringRepresantation(str2);
+		amount2++;
+	    }
 	}
 
-	@After
-	public void onAfter() { 
-		liste.clear();
-		toCompare.clear();
-		System.out.println("----- Finish Test -----\n");
+//	System.out.println("Anzahl 1 = " + amount1 + " Anzahl 2 = " + amount2);
+//	System.out.println("Unterschied = "
+//		+ alg.calculateDiffrence(alg2.getMap()));
+    }
+
+    @Test
+    public void Leven() {
+	int amount1 = 0;
+	LevenSt alg = new LevenSt();
+	for (String str : liste) {
+	    for (String str2 : str.split(" ")) {
+		alg.addString(str2);
+		amount1++;
+	    }
 	}
-
-	public void test() {
-		SoundexAlg alg = new SoundexAlg();
-		System.out.println("Unterschied Soundex = "
-				+ alg.compareStrings(liste.get(i), toCompare.get(i)));
-
-		LevenSt alg2 = new LevenSt();
-		System.out.println("Unterschied Levenshtein = "
-				+ alg2.getLevenSt(liste.get(i), toCompare.get(i)));
-
-		MetaphoneAlg alg3 = new MetaphoneAlg();
-		System.out.println("Unterschied Metaphone = "
-				+ alg3.getDiffrence(liste.get(i), toCompare.get(i)));
+	int amount2 = 0;
+	LevenSt alg2 = new LevenSt();
+	for (String str : toCompare) {
+	    for (String str2 : str.split(" ")) {
+		alg2.addString(str2);
+		amount2++;
+	    }
 	}
+//	System.out.println("Anzahl 1 = " + amount1 + " Anzahl 2 = " + amount2);
+//	System.out.println("Unterschied = "
+//		+ alg.calculateDiffrence(alg2.getList()));
+    }
 
-	@Test
-	public void test2() {
-		int amount1 = 0;
-		MetaphoneAlg alg = new MetaphoneAlg();
-		for (String str : liste) {
-			for (String str2 : str.split(" ")) {
-				alg.getStringRepresantation(str2);
-				amount1++;
-			}
+    private void readFile(boolean original) {
+	String file;
+	if (original) {
+	    file = "test.txt";
+	} else {
+	    file = "test2.txt";
+	}
+	BufferedReader br = null;
+	try {
+	    br = new BufferedReader(new FileReader(new File(file)));
+	    String line = null;
+	    while ((line = br.readLine()) != null) {
+		if (line.contains("ä")) {
+		    line = line.replaceAll("ä", "ae");
 		}
-		int amount2 = 0;
-		MetaphoneAlg alg2 = new MetaphoneAlg();
-		for (String str : toCompare) {
-			for (String str2 : str.split(" ")) {
-				alg2.getStringRepresantation(str2);
-				amount2++;
-			}
+		if (line.contains("ü")) {
+		    line = line.replaceAll("ü", "ue");
 		}
-		System.out.println("Anzahl 1 = " + amount1 + " Anzahl 2 = " + amount2);
-		System.out.println("Unterschied = "
-				+ alg.calculateDiffrence(alg2.getMap()));
-	}
-
-	@Test
-	public void test3() {
-		int amount1 = 0;
-		SoundexAlg alg = new SoundexAlg();
-		for (String str : liste) {
-			for (String str2 : str.split(" ")) {
-				alg.getStringRepresantation(str2);
-				amount1++;
-			}
+		if (line.contains("ö")) {
+		    line = line.replaceAll("ö", "oe");
 		}
-		int amount2 = 0;
-		SoundexAlg alg2 = new SoundexAlg();
-		for (String str : toCompare) {
-			for (String str2 : str.split(" ")) {
-				alg2.getStringRepresantation(str2);
-				amount2++;
-			}
+		if (line.contains("ß")) {
+		    line = line.replaceAll("ß", "ss");
 		}
 
-		System.out.println("Anzahl 1 = " + amount1 + " Anzahl 2 = " + amount2);
-		System.out.println("Unterschied = "
-				+ alg.calculateDiffrence(alg2.getMap()));
-	}
-
-	private void readFile(boolean original) {
-		String file;
 		if (original) {
-			file = "brief dobner.rtf";
+		    liste.add(line);
 		} else {
-			file = "brief dobner2.rtf";
+		    toCompare.add(line);
 		}
-		BufferedReader br = null;
+	    }
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (br != null) {
 		try {
-			br = new BufferedReader(new FileReader(new File(file)));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				if (original) {
-					liste.add(line);
-				} else {
-					toCompare.add(line);
-				}
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		    br.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+		    e.printStackTrace();
 		}
+	    }
 	}
+    }
 }
